20571d082c14f76387867929f1fbf2b7ec08eb79
Benedict <benedictumeozor@gmail.com>
2024-09-27 09:17:34 +0100

    installed shadcn ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,22 +11,29 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: "#000D50", // navy
-        accent: "#F26A52", // coral
+        primary: "#000D50",
+        accent: "#F26A52",
         lavender: "#919CFD",
         light: {
-          lavender: "#DBE1FD", // lavender
-          stone: "#868484", // stone
+          lavender: "#DBE1FD",
+          stone: "#868484",
           sky: "#D9F2FF",
         },
         fade: "#A3A3A3",
-        background: "#F5F5F5", // porcelain
+        background: "#F5F5F5",
+        "lavender-text": "#205184",
+        "deep-blue": "#0E2254",
       },
       screens: {
-        sm: "480px", // mobiles
-        md: "768px", // tablets
-        lg: "976px", // laptops
-        xl: "1440px", // desktops
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
